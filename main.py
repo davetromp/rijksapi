891d0742fb4ec9c50add3a3a3f7fdf66290c28d8
Dave Tromp <davetromp@gmail.com>
2022-02-23 11:29:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import requests
+import configparser
+
+from flask import Flask, send_file
+from flask_cors import CORS, cross_origin
+
+
+# CREATE OBJECT
+config_file = configparser.ConfigParser()
+# READ CONFIG FILE
+config_file.read("config.ini")
+# Get the api_key
+API_KEY = config_file['api']['api_key']
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def get_rijks(q):
+    url = 'https://www.rijksmuseum.nl/api/nl/collection?key={}&imgonly=True&toppieces=True&q={}&s=relevance&ps=1'.format(
+        API_KEY, q)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+
+
+def parse_rijks(data):
+    """
+    Function that will parse the rijksmuseam api response to filter out
+    title, maker and imageurl
+    """
+    result = {}
+    for artObject in data['artObjects']:
+        try:
+            result[artObject['id']] = {
+                'title': artObject['title'],
+                'maker': artObject['principalOrFirstMaker'],
+                'imageurl': artObject['webImage']['url']
+            }
+        except:
+            pass
+    return result
+
+
+@app.route("/")
+def index():
+    """
+    Using flask to serve static index.html.
+    """
+    return send_file('static/index.html')
+
+
+@app.route("/favicon.ico")
+def favicon():
+    """
+    Using flask to serve the facicon.
+    """
+    return send_file('static/favicon.ico')
+
+
+@app.route('/get/<q>')
+@cross_origin()
+def get(q):
+    data = get_rijks(q)
+    result = parse_rijks(data)
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+
+    el: '#main',
+   
+    data: {
+
+        results: "",
+        responseAvailable: false,
+        q : ""
+    },
+
+    methods: {
+
+        fetchAPIData( ) { 
+            this.responseAvailable = false;
+
+            fetch(`http://127.0.0.1:5000/get/${this.q}` , {
+                "method": "GET"
+            })
+            .then(response => { 
+                if(response.ok){
+                    return response.json()
+                } else{
+                    alert("Server returned " + response.status + " : " + response.statusText);
+                }                
+            })
+            .then(response => {
+                this.results = response;
+                this.responseAvailable = true;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            console.log(this.result);
+        }
+    }
+
+})
